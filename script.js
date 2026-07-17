6967a7a54aa3ee939dc5906ab495edd433bf9c6f
Ornella Kayigirwa <ornellakayigirwa@gmail.com>
2026-07-17 14:07:42 +0300

    chore: add script.js file for nav bar functionality

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 
-const menuButton = document.getElementById('menu-btn');
-const closeButton = document.getElementById('close-btn');
-const mobileNav = document.getElementById('mobile-nav');
+function initNav() {
+    const menuButton = document.getElementById('menu-btn');
+    const closeButton = document.getElementById('close-btn');
+    const mobileNav = document.getElementById('mobile-nav');
 
-menuButton.addEventListener('click', () => {
-    mobileNav.classList.remove('hidden');
-    mobileNav.classList.add('flex');
-    menuButton.classList.add('hidden');
-    closeButton.classList.remove('hidden')
+    menuButton.addEventListener('click', () => {
+        mobileNav.classList.remove('hidden');
+        mobileNav.classList.add('flex');
+        menuButton.classList.add('hidden');
+        closeButton.classList.remove('hidden')
 
-})
-closeButton.addEventListener('click', () => {
-    mobileNav.classList.add('hidden');
-    mobileNav.classList.remove('flex');
-    closeButton.classList.add('hidden');
-    menuButton.classList.remove('hidden');
+    })
+    closeButton.addEventListener('click', () => {
+        mobileNav.classList.add('hidden');
+        mobileNav.classList.remove('flex');
+        closeButton.classList.add('hidden');
+        menuButton.classList.remove('hidden');
 
-})
+    })
+}
