ae805aafea25d7dcf8543e58f2ea6f2c0550639c
Ornella Kayigirwa <ornellakayigirwa@gmail.com>
2026-08-08 22:49:47 +0300

    fix: correct nav toggle function to match components

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 
+
 function togglenav() {
     const menuButton = document.getElementById('menu-btn');
-    const mobileNav=document.getElementById('mobile-nav');
-        mobileNav.classList.toggle('hidden')
-        menuButton.src= mobileNav.classList.contains('hidden') ? 'menu1.png' :'close.png'
-    }
+    const closeButton = document.getElementById('close-btn');
+    const mobileNav = document.getElementById('mobile-nav');
+
+    menuButton.addEventListener('click', () => {
+        mobileNav.classList.remove('hidden');
+        mobileNav.classList.add('flex');
+        menuButton.classList.add('hidden');
+        closeButton.classList.remove('hidden')
+
+    })
+    closeButton.addEventListener('click', () => {
+        mobileNav.classList.add('hidden');
+        mobileNav.classList.remove('flex');
+        closeButton.classList.add('hidden');
+        menuButton.classList.remove('hidden');
+
+    })
+}
